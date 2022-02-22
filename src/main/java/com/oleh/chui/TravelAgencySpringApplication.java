aabd10c1461a8f267fe11c5e482cd339ea13bf19
Oleh Chui <misterchuy21@gmail.com>
2022-02-22 11:57:14 +0200

    Initial commit

---
+++ src/main/java/com/oleh/chui/TravelAgencySpringApplication.java
@@ -0,0 +1,13 @@
+package com.oleh.chui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencySpringApplication.class, args);
+    }
+
+}
